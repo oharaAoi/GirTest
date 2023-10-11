f9ee7cf1d5118de03a8c37b4c9a36851e556bd23
oharaAoi <k023g0108@g.neec.ac.jp>
2023-10-11 12:01:43 +0900

    日付の追加

--- GirTest.cpp
+++ GirTest.cpp
@@ -8,6 +8,8 @@ int main()
     std::cout << "Hello GitHub!\n";
     std::cout << "LC1A04_オオハラ_アオイ\n";
 
+    std::cout << "2023/10/11\n";
+
 }
 
 // プログラムの実行: Ctrl + F5 または [デバッグ] > [デバッグなしで開始] メニュー
